3662974a52d61b070fa0d2e19e2ed0206a14d9c9
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-09 14:03:47 -0300

    projeto finalizado com sucesso

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const btnAdd = document.getElementById("btn-add");
 const tasksContainer = document.getElementById("tasks");
 
 function adicionarElemento() {
+  const valor = input.value.trim(); // remove espaços antes/depois
+
+  if (valor === "") {
+    alert("Digite uma tarefa antes de adicionar.");
+    return; // interrompe a função
+  }
+
   const ul = document.createElement("ul");
   const li = document.createElement("li");
-  const texto = document.createTextNode(input.value);
+  const texto = document.createTextNode(valor);
   const image = document.createElement("img");
   const inputCheckbox = document.createElement("input");
 
   image.src = "../../assets/icon delete.svg";
   image.alt = "delete icon";
   image.classList.add("delete-icon");
+
+  // evento para deletar a task
+  image.addEventListener("click", () => {
+    li.remove();
+    if (ul.children.length === 0) {
+      ul.remove();
+
+      mostrarAviso("O item foi removido com sucesso!");
+    }
+  });
 
   inputCheckbox.type = "checkbox";
 
@@ -21,8 +38,30 @@ function adicionarElemento() {
   li.classList.add("task");
 
   ul.appendChild(li);
-
   tasksContainer.appendChild(ul);
+
+  input.value = ""; // limpa o campo
+}
+
+function mostrarAviso(mensagem) {
+  const aviso = document.createElement("div");
+  const icon = document.createElement("img");
+  const texto = document.createElement("span");
+
+  icon.src = "../../assets/icon.svg";
+  icon.alt = "aviso icon";
+
+  texto.textContent = mensagem;
+
+  aviso.appendChild(icon);
+  aviso.appendChild(texto);
+
+  aviso.classList.add("aviso");
+  tasksContainer.appendChild(aviso);
+
+  setTimeout(() => {
+    aviso.remove();
+  }, 3000);
 }
 
 btnAdd.addEventListener("click", adicionarElemento);
